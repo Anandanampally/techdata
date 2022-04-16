62acaad8ad0789a38b81b6151cfdd4df200e8147
ananda annampali <anandanampally84@gmail.com>
2022-04-16 17:39:11 +0530

    remove comment

--- src/Components/CustomerDate.js
+++ src/Components/CustomerDate.js
@@ -3,7 +3,6 @@ import './CustomerDate.css'
 function CustomerDate(props) {
     // const month = props.date.toLocaleString('en-US', { month: 'long' });
     // const year = props.date.getFullYear();
-    // const day = props.date.toLocaleString('en-US', { day: '2-digit' });
 
  //   moment(date).format('dddd MMMM D Y')
     return (<div className='customer-date'>
